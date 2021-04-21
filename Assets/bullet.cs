c535508981bcc67392b5e116c0c6af07dd15c613
jamiepayn <512645@chrome.escg.ac.uk>
2021-04-21 16:31:11 +0100

    wahwah

---
+++ Assets/bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject impact;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        rb.velocity=new Vector2(-4,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        print("collided");
+        Instantiate(impact, transform.position);
+    }
+
+
+}
